28bcbef778af88f182eaa22b86d6ffce2253e082
Nikolay Polyakov <misterkola337@gmail.com>
2024-08-23 04:02:24 +0300

    feat: add loading animation for Catalog.tsx

---
+++ src/components/Loading.tsx
@@ -0,0 +1,18 @@
+export default function Loading() {
+    return (
+        <>
+            <div className="loading">
+                <div className="loading-round">
+                    <span className="r1"></span>
+                    <span className="r2"></span>
+                    <span className="r3"></span>
+                    <span className="r4"></span>
+                    <span className="r5"></span>
+                </div>
+                <div className="loading-title">
+                    <h2>Идет загрузка...</h2>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -3,10 +3,14 @@ import Header from "../components/Header";
 import ProductList from "../components/ProductList";
 import Filters from "../components/Filters";
 
+import {getProducts} from "../utils";
+import {Product} from "../@types/types";
+import Progress from "../components/Loading.tsx";
+
 import {useEffect, useState} from "react";
 import {SearchX} from "lucide-react";
-import {getProducts} from "../utils.ts";
-import {Product} from "../@types/types.ts";
+
+
 
 export default function Catalog() {
 
@@ -17,7 +21,7 @@ export default function Catalog() {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const data = await getProducts();
+                const data = getProducts();
                 setProducts(data);
             } catch (error) {
                 console.error('Failed to fetch products:', error);
@@ -29,10 +33,6 @@ export default function Catalog() {
         fetchProducts();
     }, []);
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
     return (
         <>
             <Header />
@@ -43,12 +43,13 @@ export default function Catalog() {
                     <Filters products={products} setFilteredProducts={setFilteredProducts} />
                     <div className="list">
                         <ProductList products={filteredProducts}/>
-                        {filteredProducts.length === 0 &&
+                        {filteredProducts.length === 0 && !isLoading &&
                             <div className="not-found-products">
                                 <SearchX width={100} height={100}/>
                                 <h2>Нет товаров, соответствующих выбранным фильтрам.</h2>
                             </div>
                         }
+                        {isLoading && <Progress/>}
                     </div>
                 </div>
             </div>
